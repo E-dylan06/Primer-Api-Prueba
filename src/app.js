1e4796c852e0bcdfd891fcf5070f08404f73f241
E-dylan06 <dylan2006-18@hotmail.com>
2025-07-09 00:53:07 -0500

    PrimerComit: Api

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const config=require('./config');
+const clientes=require('./modules/client/client-controller');
+const productos=require('./modules/product/product-controller');
+const app=express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('port',config.app.port);
+
+//rutas
+app.use('/api/clientes', clientes);
+app.use('/api/productos', productos);
+module.exports = app;
