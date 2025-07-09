79eff5a9b8d7dbda49a86131808f728efdbaca39
E-dylan06 <dylan2006-18@hotmail.com>
2025-07-09 04:48:37 -0500

    cors implementados esperemos que funcione

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
-const express=require('express');
-const config=require('./config');
-const clientes=require('./modules/client/client-controller');
-const productos=require('./modules/product/product-controller');
-const app=express();
-
-
+const express = require('express');
+const cors = require('cors');          
+const config = require('./config');
+const clientes = require('./modules/client/client-controller');
+const productos = require('./modules/product/product-controller');
+const app = express();
+app.use(cors());                  
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-app.set('port',config.app.port);
-
-//rutas
+app.use(express.urlencoded({ extended: true }));
+app.set('port', config.app.port);
 app.use('/api/clientes', clientes);
 app.use('/api/productos', productos);
+
 module.exports = app;
